1c07dd8093af3ad2f45442dec1b0ac98a2385209
Mei <mc2922@gmail.com>
2015-05-01 20:29:49 -0700

    lab-12

---
+++ ivp-extend/src/lab_12/CMakeFiles/ArithmeticCodec.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mit/hoverland/mei/ivp-extend/src/lab_12/arithmetic.cpp" "/home/mit/hoverland/mei/ivp-extend/src/lab_12/CMakeFiles/ArithmeticCodec.dir/arithmetic.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ASYNCHRONOUS_CLIENT"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/mit/hoverland/moos/core-moos/Core/libMOOS/include"
+  "/home/mit/hoverland/moos/core-moos/Core/libMOOS/App/include"
+  "/home/mit/hoverland/moos/core-moos/Core/libMOOS/Comms/include"
+  "/home/mit/hoverland/moos/core-moos/Core/libMOOS/DB/include"
+  "/home/mit/hoverland/moos/core-moos/Core/libMOOS/Utils/include"
+  "/home/mit/hoverland/moos/core-moos/Core/libMOOS/Thirdparty/PocoBits/include"
+  "/home/mit/hoverland/moos/core-moos/Core/libMOOS/Thirdparty/getpot/include"
+  "/home/mit/hoverland/moos/core-moos/Core/libMOOS/Thirdparty/AppCasting/include"
+  "/home/mit/hoverland/moos/geodesy-moos/libMOOSGeodesy/include"
+  "/home/mit/hoverland/goby/include"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_ipfview"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_apputil"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_helmivp"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_logic"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_behaviors"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_navplot"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_geometry"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_bhvutil"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_ufield"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_marineview"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_ivpsolve"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_ivpcore"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_genutil"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_behaviors-marine"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_ufld_hazards"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_logutils"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_ulogview"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_contacts"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_mbutil"
+  "/home/mit/hoverland/moos-ivp/ivp/src/lib_ivpbuild"
+  "/home/mit/hoverland/hover-kayak/code/marine/libraries/src"
+  "src/./protobuf"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- ivp-extend/src/lab_12/arithmetic.cpp
+++ ivp-extend/src/lab_12/arithmetic.cpp
@@ -19,54 +19,110 @@ ArithmeticCodec::ArithmeticCodec()
 
 goby::acomms::Bitset ArithmeticCodec::encode_repeated(const std::vector<goby::int32>& wire_values)
 {
-    // PLACEHOLDER - returns bitset representing range from [0, 1)
-    double lower = 0, upper = 1;    
 
-    // you'll want to find the decimal range to encode the symbols given in `wire_values`
-    // ...
+	// PLACEHOLDER - returns bitset representing range from [0, 1)
+	double lower = 0, upper = 1;
 
-    // Bitset data_bits = range_to_bits(std::make_pair(lower, upper));
-    // return data_bits;
+	// you'll want to find the decimal range to encode the symbols given in `wire_values`
 
+	//Make a map of temperatures to probabilities
+	std::map<goby::int32,std::pair<double,double> > tmap;
+	tmap[10]=std::pair<double,double>(0,71/100.0);
+	tmap[20]=std::pair<double,double>(0.89,1.0);
+	for ( int i=0; i<9; i++) {
+		tmap[11+i]=std::pair<double,double>((71+2*i)/100.0,(73+2*i)/100.0);
+	}
 
-    // an example for use in decode_repeated
-    std::bitset<6> example(std::string("101111"));
-    goby::acomms::Bitset example_bits(example.size(), example.to_ulong());
-    std::cout << "=== ARITHMETIC DEBUG: \"Encoded\" using: " << example_bits << std::endl;
-    return example_bits;
+	//Update map ranges
+	for(int i=0; i<5; i++){
+		double temp_lower = lower;
+		lower = tmap[wire_values[i]].first * (upper-lower) + temp_lower;
+		upper = tmap[wire_values[i]].second * (upper-lower) + temp_lower;
+	}
+	//std::cout << "=== ARITHMETIC DEBUG: New Range: "<<lower<<", "<<upper<< std::endl;
+
+	goby::acomms::Bitset data_bits = range_to_bits(std::make_pair(lower, upper));
+	std::cout << "=== ARITHMETIC DEBUG: \"Encoded\" using: " << data_bits << std::endl;
+	std::cout << data_bits.size() << std::endl; //Part 1a)
+	return data_bits;
 }
-    
+
 std::vector<goby::int32> ArithmeticCodec::decode_repeated(goby::acomms::Bitset* bits)
 {
-    std::cout << "=== ARITHMETIC DEBUG: Starting with " << min_size_repeated() << " bits: " << *bits << std::endl;
+	std::cout << "=== ARITHMETIC DEBUG: Starting with " << min_size_repeated() << " bits: " << *bits << std::endl;
+	std::vector<goby::int32> out;
 
-    // asking for the remaining 4 bits I encoded with
-    bits->get_more_bits(4);
-    std::cout << "=== ARITHMETIC DEBUG: All the bits: " << *bits << std::endl;
-    
-    
-    std::vector<goby::int32> out;    
-    // std::pair<double, double> result = bits_to_range(bits);
-    // PLACEHOLDER - figure out your symbols and add them (push_back) to `out`
-    // ...
+	//Instantiate Map and vectors
+	std::map<goby::int32,std::pair<double,double> > tmap;
+	std::vector<goby::int32> tvec; //temperature
+	std::vector<double> pvec,pvec_cpy; //probability
 
-    
-    return out;
+	//Fill map
+	tmap[10]=std::pair<double,double>(0,71/100.0);
+	tmap[20]=std::pair<double,double>(0.89,1.0);
+	for(int i=0;i<9;i++) {
+		tmap[11+i]=std::pair<double,double>((71+2*i)/100.0,(73+2*i)/100.0);
+	}
+
+	//Fill vectors
+	for(int i=10;i<=20;i++) {
+		tvec.push_back(i);
+	}
+	pvec.push_back(0.0);
+	pvec.push_back(0.71);
+	for(int i=0;i<9;i++) {
+		pvec.push_back(0.73+0.02*i);
+	}
+	pvec.push_back(1);
+	pvec_cpy = pvec;
+
+	//Compute
+	for(int i=0;i<5;i++){
+		int aInd = 0, bInd = -1;
+		while(aInd!=bInd){
+			std::pair<double, double> result = bits_to_range(*bits);
+
+			for(int j=1;j<pvec.size();j++){
+				if(result.first <= pvec[j] && result.first >= pvec[j-1]){
+					aInd = j;
+				}
+				if(result.second <= pvec[j] && result.second >= pvec[j-1]){
+					bInd = j;
+				}
+			}
+
+			if(aInd!=bInd){
+				bits->get_more_bits(1);
+			}
+		}
+
+		//Record answer
+		out.push_back(tvec[aInd-1]);
+
+		//Update ranges
+		double upper = pvec[aInd];
+		double lower = pvec[bInd-1];
+		for(int j=0;j<pvec.size();j++) {
+			pvec[j] = pvec_cpy[j]*(upper-lower) + lower;
+		}
+	}
+
+	return out;
 }
 
 unsigned ArithmeticCodec::min_size_repeated()
 {
-    // PLACEHOLDER - change to computed value
-    return 2;
+	// PLACEHOLDER - change to computed value
+	return ceil(1/log2(5*.71))+1;
 }
 
 
 unsigned ArithmeticCodec::max_size_repeated()
 {
-    // PLACEHOLDER - change to computed value
-    return 6;
+	// PLACEHOLDER - change to computed value
+	return ceil(1/log2(5*.02))+1;
 }
-    
+
 //
 // Methods already implemented for you ... be sure to understand what they do (see also arithmetic.h)
 //
@@ -74,70 +130,70 @@ unsigned ArithmeticCodec::max_size_repeated()
 
 unsigned ArithmeticCodec::size_repeated(const std::vector<goby::int32>& field_values)
 {
-    // calculates size using encode_repeated. This is inefficient, but will suffice
-    // for this lab
-    return encode_repeated(field_values).size();
+	// calculates size using encode_repeated. This is inefficient, but will suffice
+	// for this lab
+	return encode_repeated(field_values).size();
 }        
 
 std::pair<double, double> ArithmeticCodec::bits_to_range(goby::acomms::Bitset bits)
 {
-    // lower bound is the given bitset followed by 000000...
-    double lower = 0;
-    for(int i = 0, n = bits.size(); i < n; ++i)
-        lower += bits[i]/(pow(2.0, i+1));
+	// lower bound is the given bitset followed by 000000...
+	double lower = 0;
+	for(int i = 0, n = bits.size(); i < n; ++i)
+		lower += bits[i]/(pow(2.0, i+1));
 
-    // upper bound is the given bitset followed by 111111...
-    // remember 0.111111... = 1 in binary, like 0.999999... = 1 in decimal
-    double upper = lower + 1/(pow(2.0, bits.size()));
-        
-    return std::make_pair(lower,upper);
+	// upper bound is the given bitset followed by 111111...
+	// remember 0.111111... = 1 in binary, like 0.999999... = 1 in decimal
+	double upper = lower + 1/(pow(2.0, bits.size()));
+
+	return std::make_pair(lower,upper);
 }
 
 goby::acomms::Bitset ArithmeticCodec::range_to_bits(std::pair<double, double> range)
 {
-    double lower = range.first, upper = range.second;
-    goby::acomms::Bitset lower_bits, upper_bits;
+	double lower = range.first, upper = range.second;
+	goby::acomms::Bitset lower_bits, upper_bits;
 
-    // keep adding bits until the binary representation of lower and upper
-    // differ OR we have found an exact representation for both decimals
-    while(lower_bits == upper_bits && !(lower == 0 && upper == 0))
-    {
-        double lower_int_part, upper_int_part;
-        lower *= 2;
-        lower = modf(lower, &lower_int_part);
-        lower_bits.push_back(lower_int_part >= 1 ? 1 : 0);
-            
-        upper *= 2;
-        upper = modf(upper, &upper_int_part);
+	// keep adding bits until the binary representation of lower and upper
+	// differ OR we have found an exact representation for both decimals
+	while(lower_bits == upper_bits && !(lower == 0 && upper == 0))
+	{
+		double lower_int_part, upper_int_part;
+		lower *= 2;
+		lower = modf(lower, &lower_int_part);
+		lower_bits.push_back(lower_int_part >= 1 ? 1 : 0);
 
-        if(upper_int_part == 2) // deal with 1 case
-            upper = 1;
-        
-        upper_bits.push_back(upper_int_part >= 1 ? 1 : 0);
-    }
+		upper *= 2;
+		upper = modf(upper, &upper_int_part);
 
-    // if lower is an exact representation return it
-    if(lower == 0)
-    {
-        return lower_bits;
-    }
-    else // keep adding bits until we uniquely identify the range
-    {
-        while(bits_to_range(upper_bits).second > range.second)
-            upper_bits.push_back(0);
+		if(upper_int_part == 2) // deal with 1 case
+			upper = 1;
 
-        while(bits_to_range(lower_bits).first < range.first)
-            lower_bits.push_back(1);
+		upper_bits.push_back(upper_int_part >= 1 ? 1 : 0);
+	}
 
-        // return the smaller of the two representations of the range
-        return (lower_bits.size() < upper_bits.size()) ? lower_bits : upper_bits;
-    }
-        
+	// if lower is an exact representation return it
+	if(lower == 0)
+	{
+		return lower_bits;
+	}
+	else // keep adding bits until we uniquely identify the range
+	{
+		while(bits_to_range(upper_bits).second > range.second)
+			upper_bits.push_back(0);
+
+		while(bits_to_range(lower_bits).first < range.first)
+			lower_bits.push_back(1);
+
+		// return the smaller of the two representations of the range
+		return (lower_bits.size() < upper_bits.size()) ? lower_bits : upper_bits;
+	}
+
 }
 
 void ArithmeticCodec::validate()
 {
-    DCCLFieldCodecBase::require(DCCLFieldCodecBase::dccl_field_options().max_repeat() <= LARGEST_MAX_REPEAT_SIZE,
-                                "(goby.field).dccl.max_repeat must be less than or equal to " +
-                                goby::util::as<std::string>(static_cast<int>(LARGEST_MAX_REPEAT_SIZE)));
+	DCCLFieldCodecBase::require(DCCLFieldCodecBase::dccl_field_options().max_repeat() <= LARGEST_MAX_REPEAT_SIZE,
+			"(goby.field).dccl.max_repeat must be less than or equal to " +
+			goby::util::as<std::string>(static_cast<int>(LARGEST_MAX_REPEAT_SIZE)));
 }
